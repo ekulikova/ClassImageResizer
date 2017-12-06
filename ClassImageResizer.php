a954489fabf420ace07044c2ba475c4c2a52b130
ekulikova <ek270285@gmail.com>
2017-12-06 13:42:14 -0600

    First test method

--- ClassImageResizer.php
+++ ClassImageResizer.php
@@ -7,105 +7,105 @@ class imageResizer{
 	private $width;
 	private $height;
 	private $type;
-	
-	
+
+
 	public function __construct($source){
-		
+
 		 if (!is_file($source)) {
             throw new ImageResizerException('File '.$source.' does not exist');
         }
-        
+
 		$this->source=$source;
 		list($this->width, $this->height, $this->type) = getimagesize($source);
-		
+
 		if(!$this->width || !$this->height){
 			throw new ImageResizerException('File '.$source.' is not an image');
 		}
-				 
+
 		if( $this->type == IMAGETYPE_JPEG ) {
-			$this->image = imagecreatefromjpeg($source);       
+			$this->image = imagecreatefromjpeg($source);
 		} elseif( $this->type == IMAGETYPE_GIF ) {
-			$this->image = imagecreatefromgif($source);       
+			$this->image = imagecreatefromgif($source);
 		} elseif( $this->type == IMAGETYPE_PNG ) {
-			$this->image = imagecreatefrompng($source);       
+			$this->image = imagecreatefrompng($source);
 		} else {
 			throw new ImageResizerException('Unsupported image type. File '.$source);
 		}
-		
+
 		if (!$this->image) {
             throw new ImageResizerException('Could not load image from '.$source);
         }
-        
+
 	}
-		
+
 	public function __destruct(){
 		imagedestroy($this->image);
 	}
-	
-	public function update($new_image){
-		
+
+	private function update($new_image){
+
 		$this->image=$new_image;
 		$this->height=imagesy($this->image);
 		$this->widht=imagesx($this->image);
-		
+
 	}
-		
-	public function save($filename,$compression=75,$permissions=0777){
-		
+
+	private function save($filename,$compression=75,$permissions=0777){
+
 		$filename or $filename=$this->source;
-		
+
 		if( $this->type == IMAGETYPE_JPEG ) {
-			imagejpeg($this->image,$filename,$compression);       
+			imagejpeg($this->image,$filename,$compression);
 		} elseif( $this->type == IMAGETYPE_GIF ) {
-			imagegif($this->image,$filename);       
+			imagegif($this->image,$filename);
 		} elseif( $this->type == IMAGETYPE_PNG ) {
-			imagepng($this->image,$filename);       
-		}       
-			
-		if($permissions) {
-			chmod($filename,$permissions);       
+			imagepng($this->image,$filename);
 		}
-		
+
+		if($permissions) {
+			chmod($filename,$permissions);
+		}
+
 	}
-		
+
 	public function resize($new_width,$new_height){
-			
+
 		$new_image = imagecreatetruecolor($new_width, $new_height);
 		imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $new_width, $new_height, $this->width, $this->height);
 		$this->update($new_image);
-				
+
 	}
-	
+
 	public function resizeToHeight($h,$skip_small=1){
-		
+
 		if($skip_small && $this->height<=$h){
 			return;
 		}
 		else{
 			$ratio = $this->height/$h;
 			$new_height=$this->height/$ratio;
-			
+
 			$this->resize($this->width,$new_height);
 		}
-		
+
 	}
-	
+
 	public function resizeToWidth($w,$skip_small=1){
-		
+
 		if($skip_small && $this->width<=$w){
 			return;
 		}
 		else{
 			$ratio = $this->width/$w;
 			$new_width=$this->width/$ratio;
-			
+
 			$this->resize($new_width,$this->height);
 		}
-		
+
 	}
-	
+
 	public function resizeToHeightWidth($w,$h,$skip_small=1){
-		
+
 		if($skip_small && $this->width<=$w && $this->height<=$h){
 			return;
 		}
@@ -113,12 +113,12 @@ class imageResizer{
 			$ratio = max($this->width/$w,$this->height/$h);
 			$new_width=$this->width/$ratio;
 			$new_height=$this->height/$ratio;
-			
+
 			$this->resize($new_width,$new_height);
 		}
-		
+
 	}
-	
+
 }
 
 class ImageResizerException extends \Exception
--- ClassImageResizerTest.php
+++ ClassImageResizerTest.php
@@ -9,4 +9,47 @@ if (version_compare(PHP_VERSION, '7.0.0') >= 0 && !class_exists('PHPUnit_Framewo
 class imageResizerTest extends PHPUnit_Framework_TestCase
 {
 
+  private $image_types = array(
+      'gif',
+      'jpeg',
+      'png'
+  );
+
+  /**
+   * Loading tests
+   */
+
+   public function testLoadGif()
+   {
+       $image = $this->createImage(1, 1, 'gif');
+       $resize = new imageResizer($image);
+
+       $this->assertInstanceOf('imageResizer', $resize);
+   }
+
+   /**
+    * Helpers
+    */
+
+    private function createImage($width, $height, $type)
+    {
+        if (!in_array($type, $this->image_types)) {
+            throw new ImageResizeException('Unsupported image type');
+        }
+
+        $image = imagecreatetruecolor($width, $height);
+
+        $filename = $this->getTempFile();
+
+        $output_function = 'image' . $type;
+        $output_function($image, $filename);
+
+        return $filename;
+    }
+
+    private function getTempFile()
+    {
+        return tempnam(sys_get_temp_dir(), 'resize_test_image');
+    }
+
 }
