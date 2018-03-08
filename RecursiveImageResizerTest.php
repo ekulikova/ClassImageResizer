1acdba4ebf297caebd5fa35c9147be755579214e
ekulikova <ek270285@gmail.com>
2018-03-08 14:38:09 -0600

    create whole structure for test

--- RecursiveImageResizerTest.php
+++ RecursiveImageResizerTest.php
@@ -13,6 +13,15 @@ if (version_compare(PHP_VERSION, '7.0.0') >= 0 && !class_exists('PHPUnit_Framewo
 class RecursiveImageResizerTest extends PHPUnit_Framework_TestCase
 {
 
+	private $structure = array(
+		'dirs' => [],
+		'files' => [],
+	);
+
+	private $orig_width = 500;
+	private $orig_height = 500;
+	private $img_type = 'jpeg';
+
   /**
 	 * Helpers
 	 */
@@ -28,17 +37,85 @@ class RecursiveImageResizerTest extends PHPUnit_Framework_TestCase
       return $tmp_dir;
    }
 
+	 private function getTempFile($dirName)
+	 {
+		 $tmp_file = tempnam($dirName, 'RecursiveImageResizerTest');
+
+		 return $tmp_file;
+	 }
+
+	 private function createImage($dirName){
+
+		 $image = imagecreatetruecolor($this->orig_width, $this->orig_height);
+
+		 $filename = $this->getTempFile($dirName);
+
+		 $output_function = 'image' . $this->img_type;
+
+		 if( $output_function($image, $filename)){
+
+			 $this->structure['files'][] = $filename;
+
+			 return $filename;
+
+		 }
+
+		 return false;
+
+	 }
+
+	 private function createImages($dirName, $quantity){
+
+		 for( $i=1; $i<=$quantity; $i++ ){
+
+			 	$this->createImage($dirName);
+
+		 }
+
+	 }
+
+	 private function createDir($dirName){
+
+		 if( mkdir($dirName) ){
+
+				 $this->structure['dirs'][] = $dirName;
+
+				 return $dirName;
+
+		 }
+
+		 return false;
+
+	 }
+
    private function createStructure($depth, $quantity){
 
       for($i = 1; $i <= $depth; $i++){
-        // create dir
-        //mkdir($this->getDirName());
-        echo "{$this->getDirName($i)}\n";
-        // cd to it
+        
+				$dirName = $this->getDirName($i);
+
+				if ( $this->createDir( $dirName ) ){
+
+					$this->createImages($dirName, $quantity);
+
+				}
 
       }
 
    }
+
+	 private function destroyStructure(){
+
+		 	// foreach delete all files
+
+			//foreach delete all dirs
+			foreach( array_reverse( $this->structure['dirs'] ) as $dir ){
+
+					rmdir($dir);
+
+			}
+
+	 }
 
    /**
    * Tests
@@ -54,6 +131,10 @@ class RecursiveImageResizerTest extends PHPUnit_Framework_TestCase
 
       $this->assertEquals( count($images) ,6 );
 */
+
+var_dump ($this->structure);
+
+			//$this->destroyStructure();
    }
 
   /**
